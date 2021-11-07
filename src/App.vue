3b5b84edafeb100d677b27903e09166450939b12
zhuqinfei <1586859762@qq.com>
2021-11-07 23:59:39 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hi
+  </div>
+  <Mango/>
+</template>
+
+<script>
+import Mango from './components/Mango.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Mango
+  }
+}
+</script>
