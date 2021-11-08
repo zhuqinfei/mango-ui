90d5b08cdb61aadc8e812f467edcf731cf5db2c3
zhuqinfei <1586859762@qq.com>
2021-11-08 17:58:56 +0800

    搭建HTML结构

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory,createRouter} from 'vue-router'
-import Mango from './components/Mango.vue'
-import Mango2 from './components/Mango2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history =createWebHashHistory()
 const router= createRouter({
     history:history,
     routes:[
-        {path:'/',component:Mango},
-        {path:'/xxx',component:Mango2}
+        {path:'/',component:Home},
+        {path:'/doc',component:Doc}
     ]
 })
 
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>芒果UI</h1>
+            <h2>一个框架</h2>
+            <p class="aciton">
+                <a href>Github</a>
+                <a href>开始</a>
+            </p>
+        </div>
+    </div>
+</template>
