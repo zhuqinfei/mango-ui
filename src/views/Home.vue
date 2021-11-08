e2db8563cc6776e015435108b0f14daa82ad3fee
zhuqinfei <1586859762@qq.com>
2021-11-08 21:43:23 +0800

    Home页面的CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,9 @@
         <div class="banner">
             <h1>芒果UI</h1>
             <h2>一个框架</h2>
-            <p class="aciton">
-                <a href>Github</a>
-                <a href>开始</a>
+            <p class="actions">
+                <a>GitHub</a>
+                <a>开始</a>
             </p>
         </div>
     </div>
@@ -16,12 +16,32 @@
 import Topnav from "../components/Topnav.vue"
 
 export default {
-    components:{
+    components: {
         Topnav
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions {
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
 </style>
