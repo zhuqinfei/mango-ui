5e38be4d599a5415da7952d30f4ee0265117168b
zhuqinfei <1586859762@qq.com>
2021-11-09 23:16:31 +0800

    aside 的 css1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-        <Topnav />
-        <div class="content">
-            <aside>
+    <Topnav />
+    <div class="content">
+      <aside>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -19,21 +19,36 @@
           </li>
         </ol>
       </aside>
-            <main>主内容</main>
-        </div>
+      <main>主内容</main>
     </div>
+  </div>
 </template>
 
 <script lang='ts'>
 import Topnav from "../components/Topnav.vue"
 
 export default {
-    components:{
-        Topnav
-    }
+  components: {
+    Topnav
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
