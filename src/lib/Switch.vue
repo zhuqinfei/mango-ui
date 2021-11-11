b634a708125702f3775c977596056874c2176bd5
zhuqinfei <1586859762@qq.com>
2021-11-11 22:46:16 +0800

    按钮实现动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -37,6 +37,7 @@ export default {
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
+    transition:left 250ms;
   }
    button.checked{
     background: blue;
@@ -44,4 +45,7 @@ export default {
   button.checked >span{
     left: calc(100% - #{$h2} - 2px);
   } 
+  button:focus{
+      outline:none;
+  }
 </style>
