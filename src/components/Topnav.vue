280b66e129fedd3c813826779af7f29415091831
zhuqinfei <1586859762@qq.com>
2021-11-11 16:29:10 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleMenu">LOGO</div>
+        <div class="logo">LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
-        <span class="toggleAside"></span>
+        <span class="toggleAside"  @click="toggleMenu"></span>
     </div>
 </template>
 
@@ -54,10 +54,12 @@ export default {
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
+    display:none;
   }
     @media(max-width:500px){
         >.menu{display: none;}
         >.logo{margin:0 auto;}
+        >.toggleAside{display: inline-block;}
     }
 }
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -29,10 +29,10 @@ import { inject, Ref } from 'vue'
 import Topnav from "../components/Topnav.vue"
 
 export default {
-  components: {Topnav},
-   setup(){
-     const asideVisible = inject<Ref<boolean>>('asideVisible')   //get
-     return {asideVisible}
+  components: { Topnav },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible')   //get
+    return { asideVisible }
   }
 }
 </script>
@@ -42,10 +42,7 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top:70px;
+
   > h2 {
     margin-bottom: 4px;
   }
@@ -53,6 +50,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style>
