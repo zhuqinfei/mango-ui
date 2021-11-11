abcabf191abeff3d964c5b3a45228eaaa9e61a69
zhuqinfei <1586859762@qq.com>
2021-11-11 15:51:11 +0800

    让topnav是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -30,6 +30,8 @@ export default {
     padding: 16px;
     position: relative;
     z-index: 10;
+    justify-content: center;
+    align-items: center;
     > .logo {
         max-width: 6em;
         margin-right: auto;
@@ -41,6 +43,10 @@ export default {
         > li {
             margin: 0 1em;
         }
+    }
+    @media(max-width:500px){
+        >.menu{display: none;}
+        >.logo{margin:0 auto;}
     }
 }
 </style>
