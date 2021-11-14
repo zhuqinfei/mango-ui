64e82a191d5293cae9e59edc767837dc060b497d
zhuqinfei <1586859762@qq.com>
2021-11-14 23:19:23 +0800

    button绑定事件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,11 @@
     </div>
     <h1>示例1</h1>
     <div>
-        <Button>你好</Button>
+        <Button @click="onClick"
+                @focus="onClick"
+                @mouseover="onClick"
+                size="small"
+        >你好</Button>
     </div>
 </template>
 
@@ -12,6 +16,12 @@
 import Button from '../lib/Button.vue'
 
 export default {
-    components:{Button}
+    components:{Button},
+    setup(){
+      const onClick=()=>{
+        console.log('hi')
+      }
+      return {onClick}
+    }
 }
 </script>
