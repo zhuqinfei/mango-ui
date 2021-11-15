fb914122130451bdb49a47bcd23a5243466efad4
zhuqinfei <1586859762@qq.com>
2021-11-15 23:25:57 +0800

    添加theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,17 @@
 <template>
-    <div :size="size">
-        <button v-bind="rest">
+        <button class="mango-button"
+           :class="`theme-${theme}`"
+        >
             <slot />
         </button>
-    </div>
 </template>
 <script lang="ts">
 export default {
-    inheritAttrs:false,
-    setup(prop,context){
-       const {size,...rest} = context.attrs
-       return {size,rest}
+    props:{
+        theme:{
+            type:String,
+            default:'button'
+        }
     }
 }
 </script>
