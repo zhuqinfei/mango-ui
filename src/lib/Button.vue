1eab6a81ab1e4b1858fe9c6ac8b03c4d02e0be33
zhuqinfei <1586859762@qq.com>
2021-11-16 18:29:14 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,31 +1,36 @@
 <template>
-        <button class="mango-button" :class="classes">
-            <slot />
-        </button>
+  <button class="mango-button" :class="classes">
+    <slot />
+  </button>
 </template>
 <script lang="ts">
 import { computed } from 'vue';
 export default {
-    props:{
-        theme:{
-            type:String,
-            default:"button",
-        },
-        size:{
-          type:String,
-          default:"normal"
-        }
+  props: {
+    theme: {
+      type: String,
+      default: "button",
     },
-    setup(props){
-      const { theme , size} = props
-     const classes = computed(()=>{
-          return {
-            [`mango-theme-${theme}`]:theme,
-            [`mango-size-${size}`]:size,
-            };
-     })
-     return {classes}
-    }
+    size: {
+      type: String,
+      default: "normal"
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const { theme, size , level} = props
+    const classes = computed(() => {
+      return {
+        [`mango-theme-${theme}`]: theme,
+        [`mango-size-${size}`]: size,
+        [`mango-level-${level}`]: level,
+      };
+    })
+    return { classes }
+  }
 }
 </script>
 <style lang="scss">
@@ -34,6 +39,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .mango-button {
   box-sizing: border-box;
   height: $h;
@@ -48,6 +54,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -62,31 +69,80 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-   &.mango-theme-link{
+  &.mango-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.mango-theme-text{
+  &.mango-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
     }
   }
-    &.mango-size-big{
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px
+  &.mango-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.mango-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+   &.mango-theme-button {
+    &.mango-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.mango-size-small{
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.mango-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
     }
+  }
+  &.mango-theme-link {
+    &.mango-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.mango-theme-text {
+    &.mango-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.mango-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
 </style>
