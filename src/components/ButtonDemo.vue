62807bb08e77002a9e0c6018e55e061b1aa79085
zhuqinfei <1586859762@qq.com>
2021-11-16 18:09:41 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,26 +1,30 @@
 <template>
-    <div>
-        Button示例
-    </div>
-    <h1>示例1</h1>
-    <div>
-        <Button>你好</Button>
-        <Button theme="button">你好</Button>
-        <Button theme="link">你好</Button>
-        <Button theme="text">你好</Button>
-    </div>
+  <div>Button示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 
 export default {
-    components:{Button},
-    setup(){
-      const onClick=()=>{
-        console.log('hi')
-      }
-      return {onClick}
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('hi')
     }
+    return { onClick }
+  }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,17 +1,30 @@
 <template>
-        <button class="mango-button"
-           :class="`mango-theme-${theme}`"
-        >
+        <button class="mango-button" :class="classes">
             <slot />
         </button>
 </template>
 <script lang="ts">
+import { computed } from 'vue';
 export default {
     props:{
         theme:{
             type:String,
             default:"button",
+        },
+        size:{
+          type:String,
+          default:"normal"
         }
+    },
+    setup(props){
+      const { theme , size} = props
+     const classes = computed(()=>{
+          return {
+            [`mango-theme-${theme}`]:theme,
+            [`mango-size-${size}`]:size,
+            };
+     })
+     return {classes}
     }
 }
 </script>
@@ -63,6 +76,18 @@ $radius: 4px;
     color: inherit;
     &:hover,&:focus{
       background: darken(white, 5%);;
+    }
+  }
+    &.mango-theme-button{
+    &.mango-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.mango-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
     }
   }
 }
