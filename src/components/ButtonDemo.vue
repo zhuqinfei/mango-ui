0373234e7e8e1237a32fa190454a095414b2d302
zhuqinfei <1586859762@qq.com>
2021-11-17 15:50:02 +0800

    基本完善 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,6 +3,7 @@
   <h1>示例1</h1>
   <div>
     <Button>你好</Button>
+    <Button @click="onClick">你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
