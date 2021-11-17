5e11ccd63151bc4782d39395db12eed195692032
zhuqinfei <1586859762@qq.com>
2021-11-17 16:34:05 +0800

    让Dialog支持visible属性

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,26 +1,34 @@
 <template>
-    <div class="mango-dialog-overlay"></div>
-    <div class="mango-dialog-wrapper">
-        <div class="mango-dialog">
-            <header>
-                标题
-                <span class="mango-dialog-close"></span>
-            </header>
-            <main>
-                <p>第一行字</p>
-                <p>第二行字</p>
-            </main>
-            <footer>
-                <Button level="main">OK</Button>
-                <Button>Cancel</Button>
-            </footer>
+    <template v-if="visible">
+        <div class="mango-dialog-overlay"></div>
+        <div class="mango-dialog-wrapper">
+            <div class="mango-dialog">
+                <header>
+                    标题
+                    <span class="mango-dialog-close"></span>
+                </header>
+                <main>
+                    <p>第一行字</p>
+                    <p>第二行字</p>
+                </main>
+                <footer>
+                    <Button level="main">OK</Button>
+                    <Button>Cancel</Button>
+                </footer>
+            </div>
         </div>
-    </div>
+    </template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue"
 export default {
+    props: {
+        visible: {
+            type: Boolean,
+            default: false,
+        }
+    },
     components: { Button }
 }
 </script>
