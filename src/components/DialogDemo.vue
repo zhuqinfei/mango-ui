8712a32843eecf8b885b72da7d927eccb2c9ecf4
zhuqinfei <1586859762@qq.com>
2021-11-17 15:58:00 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        Dialog示例
-    </div>
+    <div>Dialog示例</div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue"
+
+export default {
+    components:{Dialog}
+}
+</script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        Dialog组件
-    </div>
+   <div class="dialog-overlay"></div>
+   <div class="dialog-wrapper">
+       <header>标题</header>
+       <main>
+           <p>第一行字</p>
+           <p>第二行字</p>
+       </main>
+       <footer>
+           <Button>OK</Button>
+           <Button>Cancel</Button>
+       </footer>
+   </div>
 </template>
+<script lang="ts">
+import Button from "./Button.vue"
+export default {
+    components:{Button}
+}
+</script>
