1d8d152fc67eca21ef536b82a5a57329090a233f
zhuqinfei <1586859762@qq.com>
2021-11-22 02:23:49 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,7 +3,7 @@
         <div class="mango-tabs-nav" ref="container">
             <div class="mango-tabs-nav-item"
                 v-for="(t,index) in titles"
-                :ref="el => { if (el) navItems[index] = el }"
+                :ref="el => { if (t===selected) selectedItem= el }"
                 @click="select(t)"
                 :class="{ selected: t === selected }"
                 :key="index"
@@ -33,16 +33,14 @@ export default {
         }
     },
     setup(props, context) {
-        const navItems=ref<HTMLDivElement[]>([])
+        const selectedItem=ref<HTMLDivElement>(null)
         const indicator=ref<HTMLDivElement>(null)
         const container=ref<HTMLDivElement>(null)
         const x=()=>{
-           const divs=navItems.value
-           const result=divs.filter(div=>div.classList.contains('selected'))[0]
-           const {width}=result.getBoundingClientRect() //获取元素宽度
+           const {width}=selectedItem.value.getBoundingClientRect() //获取元素宽度
            indicator.value.style.width=width+"px"
            const {left:left1}=container.value.getBoundingClientRect()
-           const {left:left2}=result.getBoundingClientRect()
+           const {left:left2}=selectedItem.value.getBoundingClientRect()
            const left=left2-left1
            indicator.value.style.left=left+"px"
         }
@@ -65,7 +63,7 @@ export default {
         const select = (title: string) => {
             context.emit('update:selected', title)
         }
-        return { defaults, titles, current, select,navItems,indicator,container}
+        return { defaults, titles, current, select,selectedItem,indicator,container}
     }
 }
 </script>
