b5c4aa76b9f2d8c7f5c03dc8e61218fa1e096de3
zhuqinfei <1586859762@qq.com>
2021-11-21 18:48:14 +0800

    Tabs的子组件l类型必须是Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,8 +1,7 @@
 <template>
-    <div>Tabs 示例</div>
-    <h1>示例1</h1>
+ 
     <Tabs>
-        <Tab title="导航1">内容1</Tab>
+        <div title="导航1">内容1</div>
         <Tab title="导航2">内容2</Tab>
     </Tabs>
 </template>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>Tabs 组件</div>
+    <div>
+        Tabs 组件
+        <component :is="defaults[0]"></component>
+        <component :is="defaults[1]"></component>
+    </div>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+    setup(props,context){
+       const defaults=context.slots.default()
+       defaults.forEach((tag)=>{
+            if(tag.type !== Tab){
+                throw new Error('Tab 子标签必须是 Tab')
+            }
+       })
+       return{defaults}
+    }
+}
+</script>
