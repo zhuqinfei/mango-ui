5a8c47e10828a82113118c2425b2a5823c4457a4
zhuqinfei <1586859762@qq.com>
2021-11-22 16:20:56 +0800

    从 iconfont.cn 引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="topnav">
-        <div class="logo">LOGO</div>
+        <div class="logo">
+            <svg class="icon">
+                <use xlink:href="#icon-mango" />
+            </svg>
+        </div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
-        <span class="toggleAside"  @click="toggleMenu"></span>
+        <span class="toggleAside" @click="toggleMenu"></span>
     </div>
 </template>
 
@@ -14,30 +18,36 @@ import { inject, Ref } from 'vue'
 
 
 export default {
-  setup(){
-     const asideVisible = inject<Ref<boolean>>('asideVisible')   //get
-    const toggleMenu=()=>{
-        asideVisible.value=!asideVisible.value
+    setup() {
+        const asideVisible = inject<Ref<boolean>>('asideVisible')   //get
+        const toggleMenu = () => {
+            asideVisible.value = !asideVisible.value
+        }
+        return { toggleMenu }
     }
-    return {toggleMenu}
-  }
 }
 </script>
 
 <style lang="scss">
+$color: #007974;
 .topnav {
+    color: $color;
     display: flex;
     padding: 16px;
     position: fixed;
     top: 0;
     left: 0;
-    width:100%;
+    width: 100%;
     z-index: 10;
     justify-content: center;
     align-items: center;
     > .logo {
         max-width: 6em;
         margin-right: auto;
+        > svg{
+           width:32px;
+           height: 32px;
+        }
     }
     > .menu {
         display: flex;
@@ -48,20 +58,26 @@ export default {
         }
     }
     > .toggleAside {
-    display: inline-block;
-    width: 24px;
-    height: 24px;
-    background: red;
-    position: absolute;
-    left: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-    display:none;
-  }
-    @media(max-width:500px){
-        >.menu{display: none;}
-        >.logo{margin:0 auto;}
-        >.toggleAside{display: inline-block;}
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+        display: none;
+    }
+    @media (max-width: 500px) {
+        > .menu {
+            display: none;
+        }
+        > .logo {
+            margin: 0 auto;
+        }
+        > .toggleAside {
+            display: inline-block;
+        }
     }
 }
 </style>
