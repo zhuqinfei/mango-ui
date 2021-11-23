3baeaedfd27b7238262d3bed68387b04ecb0cbdd
zhuqinfei <1586859762@qq.com>
2021-11-24 01:05:51 +0800

    事不过三：优化 markdown

--- src/router.ts
+++ src/router.ts
@@ -9,9 +9,11 @@ import DocDemo  from './components/DocDemo.vue'
 import Intro from './views/Intro.vue';
 import GetStarted from './views/GetStarted.vue'
 import Install from './views/Install.vue'
-
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory()
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
     history: history,
     routes: [
@@ -20,9 +22,9 @@ export const router = createRouter({
             path: '/doc', component: Doc, 
             children: [  
                 { path: '', component: DocDemo},
-                { path: 'intro', component: Intro },
-                { path: 'get-started', component: GetStarted },
-                { path: 'install', component: Install },
+                { path: "intro", component: md('intro') },
+                { path: "get-started", component: md('get-started') },
+                { path: "install", component: md('install') },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
