51a6e1335b6967f1486d47f069ad1f5090f6bf0d
zhuqinfei <1586859762@qq.com>
2021-11-25 23:26:29 +0800

    解决yarn build之后bug

--- src/router.ts
+++ src/router.ts
@@ -5,15 +5,14 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo  from './components/TabsDemo.vue'
-import DocDemo  from './components/DocDemo.vue'
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import intro from './markdown/intro.md'
+import getStarted from './markdown/get-started.md'
+import install from './markdown/install.md'
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content:string, key: string})
 export const router = createRouter({
     history: history,
     routes: [
@@ -22,9 +21,9 @@ export const router = createRouter({
             path: '/doc', component: Doc, 
             children: [  
                 { path: '', redirect:'/doc/intro'},
-                { path: "intro", component: md('intro') },
-                { path: "get-started", component: md('get-started') },
-                { path: "install", component: md('install') },
+                { path: "intro", component: md(intro) },
+                { path: "get-started", component: md(getStarted) },
+                { path: "install", component: md(install) },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
