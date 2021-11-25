9c67096ec6c875d380f19dd1a06aba2d1360f43b
zhuqinfei <1586859762@qq.com>
2021-11-25 21:51:57 +0800

    优化并显示demo标题

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,27 +1,30 @@
 <template>
-    <div>
-        <h1>Switch 组件示例</h1>
-        <Demo :component="Switch1Demo"/>
-        <Demo :component="Switch2Demo"/>
-    </div>
+    <div class="demo">
+            <h2>{{component.__sourceCodeTitle}}</h2>
+            <div class="demo-component">
+                <component :is="component" />
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
+            </div>
+        </div>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
-import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
-import Switch1Demo from './Switch1.demo.vue'
-import Switch2Demo from './Switch2.demo.vue'
 import 'prismjs';
 import 'prismjs/themes/prism.css'
 const Prism = (window as any).Prism
-import Demo from "./Demo.vue"
 
 export default {
-    components: {Button,Demo},
-    setup() {
-        const bool = ref(false)
-        return { bool,Switch1Demo,Switch2Demo, Prism}
+    props:{
+        component:Object
+    },
+    setup(){
+       return{Prism}
     }
 }
 </script>
@@ -53,4 +56,3 @@ $border-color: #d9d9d9;
     }
   }
 }
-</style>
